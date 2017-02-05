c75fea5c4e278864864fb014ba603089ea4bb246
Liss <irina.fox88@gmail.com>
2017-02-05 17:39:10 +0200

    Refactoring buttons view

--- ListStudents/scripts/ViewButtons.js
+++ ListStudents/scripts/ViewButtons.js
@@ -4,65 +4,28 @@
 
 //View buttons on the screen
 function viewButtons () {
-    var infoContent = document.getElementById('info');
+    var infoContent = document.getElementById('info'),
+        buttonIrina = document.createElement('input'),
+        buttonSvetlana = document.createElement('input'),
+        buttonPolina = document.createElement('input'),
+        buttonElena = document.createElement('input'),
+        buttonAlexey = document.createElement('input'),
+        buttonVladimir = document.createElement('input');
 
-    var buttonIrina = document.createElement('input');
-    buttonIrina.setAttribute('id','buttonIrina');
-    buttonIrina.value = 'more';
-    buttonIrina.type = 'button';
-    infoContent.appendChild(buttonIrina);
-
-    infoContent.innerHTML += "<br/><br/>";
-
-    var buttonSvetlana = document.createElement('input');
-    buttonSvetlana.setAttribute('id','buttonSvetlana');
-    buttonSvetlana.value = 'more';
-    buttonSvetlana.type = 'button';
-    infoContent.appendChild(buttonSvetlana);
-
-    infoContent.innerHTML += "<br/><br/>";
-
-    var buttonPolina = document.createElement('input');
-    buttonPolina.setAttribute('id','buttonPolina');
-    buttonPolina.value = 'more';
-    buttonPolina.type = 'button';
-    infoContent.appendChild(buttonPolina);
-
-    infoContent.innerHTML += "<br/><br/>";
-
-    var buttonElena = document.createElement('input');
-    buttonElena.setAttribute('id','buttonElena');
-    buttonElena.value = 'more';
-    buttonElena.type = 'button';
-    infoContent.appendChild(buttonElena);
-
-    infoContent.innerHTML += "<br/><br/>";
-
-    var buttonAlexey = document.createElement('input');
-    buttonAlexey.setAttribute('id','buttonAlexey');
-    buttonAlexey.value = 'more';
-    buttonAlexey.type = 'button';
-    infoContent.appendChild(buttonAlexey);
-
-    infoContent.innerHTML += "<br/><br/>";
-
-    var buttonVladimir = document.createElement('input');
-    buttonVladimir.setAttribute('id','buttonVladimir');
-    buttonVladimir.value = 'more';
-    buttonVladimir.type = 'button';
-    infoContent.appendChild(buttonVladimir);
-
-    infoContent.innerHTML += "<br/>";
-
+    createButton(buttonIrina,'buttonIrina');
+    createButton(buttonSvetlana,'buttonSvetlana');
+    createButton(buttonPolina,'buttonPolina');
+    createButton(buttonElena,'buttonElena');
+    createButton(buttonAlexey,'buttonAlexey');
+    createButton(buttonVladimir, 'buttonVladimir');
 }
 
-/*function lala(name) {
+//Create buttons
+function createButton(name,idName) {
     var infoContent = document.getElementById('info');
-
-    document.createElement('input');
-    name.setAttribute('id',name);
+    name.setAttribute('id',idName);
     name.value = 'more';
     name.type = 'button';
     infoContent.appendChild(name);
     infoContent.innerHTML += "<br/><br/>";
-} */
+}

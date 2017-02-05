ce0372143d0cddda333c394025194c3cd6eb3ea4
Liss <irina.fox88@gmail.com>
2017-02-05 17:27:04 +0200

    Single module with buttons

--- ListStudents/scripts/create-dom.js
+++ ListStudents/scripts/create-dom.js
@@ -18,7 +18,6 @@ function createListForm() {
         namePic = document.createElement('img'),
         genderPic = document.createElement('img');
 
-
     //Adding 'id' for having an access to elements
     genderContent.setAttribute('id', 'gender');
     namesContent.setAttribute('id', 'names');
@@ -58,54 +57,6 @@ function createListForm() {
     content.appendChild(genderContent);
     content.appendChild(skypeContent);
     content.appendChild(infoContent);
-
-    //Creating buttons
-    var buttonIrina = document.createElement('input');
-    buttonIrina.setAttribute('id','buttonIrina');
-    buttonIrina.value = 'more';
-    buttonIrina.type = 'button';
-    infoContent.appendChild(buttonIrina);
-
-    infoContent.innerHTML += "<br/><br/>";
-
-    var buttonSvetlana = document.createElement('input');
-    buttonSvetlana.setAttribute('id','buttonSvetlana');
-    buttonSvetlana.value = 'more';
-    buttonSvetlana.type = 'button';
-    infoContent.appendChild(buttonSvetlana);
-
-    infoContent.innerHTML += "<br/><br/>";
-
-    var buttonPolina = document.createElement('input');
-    buttonPolina.setAttribute('id','buttonPolina');
-    buttonPolina.value = 'more';
-    buttonPolina.type = 'button';
-    infoContent.appendChild(buttonPolina);
-
-    infoContent.innerHTML += "<br/><br/>";
-
-    var buttonElena = document.createElement('input');
-    buttonElena.setAttribute('id','buttonElena');
-    buttonElena.value = 'more';
-    buttonElena.type = 'button';
-    infoContent.appendChild(buttonElena);
-
-    infoContent.innerHTML += "<br/><br/>";
-
-    var buttonAlexey = document.createElement('input');
-    buttonAlexey.setAttribute('id','buttonAlexey');
-    buttonAlexey.value = 'more';
-    buttonAlexey.type = 'button';
-    infoContent.appendChild(buttonAlexey);
-
-    infoContent.innerHTML += "<br/><br/>";
-
-    var buttonVladimir = document.createElement('input');
-    buttonVladimir.setAttribute('id','buttonVladimir');
-    buttonVladimir.value = 'more';
-    buttonVladimir.type = 'button';
-    infoContent.appendChild(buttonVladimir);
-
-    infoContent.innerHTML += "<br/>";
-
 }
+
+
--- ListStudents/scripts/main.js
+++ ListStudents/scripts/main.js
@@ -4,6 +4,7 @@ window.onload = function () {
     var students = createStudentList();
 
     createListForm();
+    viewButtons();
     AnimationStudentList();
     addStudentToForm();
     addEventToButton(students[0], students[1], students[2], students[3], students[4], students[5]);
